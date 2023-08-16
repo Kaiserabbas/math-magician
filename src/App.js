acff1363a90e8b227f49cbe91b493fc5f1e5edda
Qaisar <kayser.abbas@gmail.com>
2023-08-16 19:22:13 +0500

    Add 'routes' and 'route' for the page routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
 import Calculator from './components/Calculator.js';
 import QuoteDisplay from './components/QuoteDisplay.js';
+import Home from './components/Home.js';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <QuoteDisplay />
-    </div>
+    <>
+      <nav className="nav-list">
+        <Link className="nav" to="/">
+          Home
+        </Link>
+        <Link className="nav" to="/calculator">
+          Calculator
+        </Link>
+        <Link className="nav" to="/quotes">
+          Quotes
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<QuoteDisplay />} />
+      </Routes>
+    </>
   );
 }
 
