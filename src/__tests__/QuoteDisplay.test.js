44d33c21c150fb9a5255682db5f13b1347449873
Qaisar <kayser.abbas@gmail.com>
2023-08-23 15:04:28 +0500

    Check for eslint errors

--- src/__tests__/QuoteDisplay.test.js
+++ src/__tests__/QuoteDisplay.test.js
@@ -28,18 +28,14 @@ describe('QuoteDisplay component', () => {
     const mockQuote = 'Mock quote content';
 
     // Mock the fetch function to return the mock data
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve([{ quote: mockQuote }]),
-      })
-    );
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve([{ quote: mockQuote }]),
+    }));
 
     render(<QuoteDisplay />);
 
     // Wait for the quote to be fetched and displayed
-    await waitFor(() =>
-      expect(screen.getByText(mockQuote)).toBeInTheDocument()
-    );
+    await waitFor(() => expect(screen.getByText(mockQuote)).toBeInTheDocument());
 
     // Ensure the fetch function was called with the correct URL and headers
     expect(fetch).toHaveBeenCalledWith(
@@ -59,10 +55,8 @@ describe('QuoteDisplay component', () => {
     render(<QuoteDisplay />);
 
     // Wait for the error message to be displayed
-    await waitFor(() =>
-      expect(
-        screen.getByText('Error: An error occurred while fetching the quote.')
-      ).toBeInTheDocument()
-    );
+    await waitFor(() => expect(
+      screen.getByText('Error: An error occurred while fetching the quote.')
+    ).toBeInTheDocument());
   });
 });
